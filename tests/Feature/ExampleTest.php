efef6910c84d1c31f6bd2bab52eff2cb1934bcd0
chris <PS237745@summacollege.nl>
2024-05-12 11:26:06 +0200

    Docker stuff

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -14,16 +14,23 @@ class ExampleTest extends TestCase
      */
     public function test_example()
     {
-        $this->post_test();
-        $this->get_test();
-
-
-
-
-
-        
+        $this->create_klant_test();
+        $this->create_artikel_test();
+        $this->read_artikel_test();
+        $this->read_klant_test();
+        $this->delete_artikel_test();
+        $this->delete_klant_test();
     }
-    public function get_test()
+
+    public function read_klant_test()
+    {
+        $response = $this->get('http://127.0.0.1:8000/api/klant');
+
+        $response->assertStatus(200);
+        print_r($response->content());
+
+    }
+    public function read_artikel_test()
     {
         $response = $this->get('http://127.0.0.1:8000/api/Artikel');
 
@@ -31,14 +38,37 @@ class ExampleTest extends TestCase
         print_r($response->content());
 
     }
-    public function post_test()
+    public function delete_artikel_test()
+    {
+        $response = $this->delete("http://127.0.0.1:8000/api/Artikel?name=312312&prijs=12312&BoodschappenlijstId=1");
+
+        $response->assertStatus(204);
+        print_r($response->content());
+    }
+    public function delete_klant_test()
+    {
+        $response = $this->delete("http://127.0.0.1:8000/api/Klant?name=hallo");
+
+        $response->assertStatus(204);
+        print_r($response->content());
+    }
+    public function create_artikel_test()
     {
         $response = $this->post("http://127.0.0.1:8000/api/Artikel?name=312312&prijs=12312&BoodschappenlijstId=1");
         //201 = Created
 
         $response->assertStatus(201);
+        print_r($response->content());
 
     }
+    public function create_klant_test()
+    {
+        $response = $this->post("http://127.0.0.1:8000/api/Klant?name=hallo");
+        //201 = Created
 
+        $response->assertStatus(201);
+        print_r($response->content());
+
+    }
 
 }
